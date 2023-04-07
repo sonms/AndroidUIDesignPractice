86444c99b5e381b207c20a492d22a3f68a25f768
sonms <smsms5676@naver.com>
2023-04-07 22:47:16 +0900

    feature/recyclerview ui practice(..ing 3)

--- app/src/main/java/com/example/myappuidesignpractice/MainActivity.kt
+++ app/src/main/java/com/example/myappuidesignpractice/MainActivity.kt
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.OvershootInterpolator
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myappuidesignpractice.databinding.ActivityMainBinding
 import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var mBinding : ActivityMainBinding
+    private lateinit var adapter: ItemAdapter
+    private var testData = arrayListOf<String?>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rv = findViewById<RecyclerView>(R.id.recyclerviewtest)
         rv.itemAnimator = SlideInUpAnimator(OvershootInterpolator(1f))
+    }
+
+    private fun initRV() {
+        adapter = ItemAdapter()
+        adapter!!.itemData = testData
+        //adapter!!.setHasStableIds(true)
     }
 }
