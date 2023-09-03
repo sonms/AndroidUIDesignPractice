405c2b8d94324be872b4aea82b444160969c07f7
sonms <smsms5676@naver.com>
2023-09-03 23:20:42 +0900

    feature/UIpractice(..ing 66 , setting test feature)

--- app/src/main/java/com/example/myappuidesignpractice/SettingsActivity.kt
+++ app/src/main/java/com/example/myappuidesignpractice/SettingsActivity.kt
@@ -3,8 +3,10 @@ package com.example.myappuidesignpractice
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 
 class SettingsActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +21,23 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+        private var switch : SwitchPreferenceCompat? = null
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            switch = findPreference("sync")
+            switch!!.setOnPreferenceChangeListener { preference, newValue ->
+                var isChecked = false
+                if (newValue as Boolean) {
+                    isChecked = newValue
+                }
+                if (isChecked) {
+                    println("tr")
+                } else {
+                   println("fa")
+                }
+                return@setOnPreferenceChangeListener true
+            }
 
 
         }
