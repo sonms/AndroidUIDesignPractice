76f27fbce308f2a8334bcfe422eac989b031d427
sonms <smsms5676@naver.com>
2023-09-04 20:21:00 +0900

    feature/UIpractice(..ing 66 , progressbar test feature)

--- app/src/main/java/com/example/myappuidesignpractice/ChattingActivity.kt
+++ app/src/main/java/com/example/myappuidesignpractice/ChattingActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myappuidesignpractice
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myappuidesignpractice.databinding.ActivityChattingBinding
@@ -11,5 +12,17 @@ class ChattingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityChattingBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        mBinding.messageSendIV.setOnClickListener {
+            //mBinding.chatPb.setProgress(70, true)
+            // ObjectAnimator를 사용하여 진행률 애니메이션 생성
+            val animator = ObjectAnimator.ofInt(mBinding.chatPb, "progress", 0, 100)
+
+            // 애니메이션 지속 시간 설정 (예: 2초)
+            animator.duration = 2000
+
+            // 애니메이션 시작
+            animator.start()
+        }
     }
 }
