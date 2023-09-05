17d67d19fd20a4890c7c952b4185f6749261b416
sonms <smsms5676@naver.com>
2023-09-05 21:06:25 +0900

    feature/UIpractice(..ing 67 , custom dialog test feature)

--- app/src/main/java/com/example/myappuidesignpractice/ScrollTestActivity.kt
+++ app/src/main/java/com/example/myappuidesignpractice/ScrollTestActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myappuidesignpractice
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myappuidesignpractice.databinding.ActivityScrollTestBinding
@@ -19,6 +22,17 @@ class ScrollTestActivity : AppCompatActivity() {
         mBinding!!.teb.setOnClickListener {
             val loadingDialog = LoadingProgressDialog(this)
             loadingDialog.window!!.setBackgroundDrawableResource(android.R.color.transparent)
+            //loadingDialog.window!!.setBackgroundDrawableResource(android.R.color.transparent)
+            //로딩창
+            loadingDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            loadingDialog.window?.attributes?.windowAnimations = R.style.FullScreenDialog // 위에서 정의한 스타일을 적용
+
+
+            loadingDialog.window!!.setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+
             loadingDialog.show()
         }
 
