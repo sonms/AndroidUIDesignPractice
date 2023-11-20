c03fa0ed0d6a33a1b200b3b023aa1ac32157b041
sonms <smsms5676@naver.com>
2023-11-21 02:04:44 +0900

    feature/UIpractice(..ing 88 , timePickerDialog Test and status bar test)

--- app/src/main/java/com/example/myappuidesignpractice/ChipAndBottomNavigationTestActivity.kt
+++ app/src/main/java/com/example/myappuidesignpractice/ChipAndBottomNavigationTestActivity.kt
@@ -1,20 +1,83 @@
 package com.example.myappuidesignpractice
 
+import android.app.TimePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.example.myappuidesignpractice.databinding.ActivityChipAndBottomNavigationTestBinding
 import com.google.android.material.chip.Chip
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class ChipAndBottomNavigationTestActivity : AppCompatActivity() {
     private lateinit var binding : ActivityChipAndBottomNavigationTestBinding
     //칩 생성
     private var chipList = kotlin.collections.ArrayList<Chip>()
-
+    private var selectFormattedTime : String? = ""
+    private var selectTime : String? = ""
+    private var hour1 = 0
+    private var minute1 = 0
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChipAndBottomNavigationTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
         tempList()
+
+        binding.datePickerActionsBtm.setOnClickListener {
+            val myCalender = Calendar.getInstance()
+            val hour = myCalender[Calendar.HOUR_OF_DAY]
+            val minute = myCalender[Calendar.MINUTE]
+            val myTimeListener =
+                TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                    if (view.isShown) {
+                        myCalender[Calendar.HOUR_OF_DAY] = hourOfDay
+                        myCalender[Calendar.MINUTE] = minute
+                        val tempS = hourOfDay.toString() + "시 " + minute + "분"
+                        selectFormattedTime = LocalTime.parse(tempS, DateTimeFormatter.ofPattern("H시 m분")).format(
+                            DateTimeFormatter.ofPattern("HH:mm"))
+
+                        selectTime = if (hourOfDay > 12) {
+                            val pm = hourOfDay - 12;
+                            "오후 " + pm + "시 " + minute + "분"
+                        } else {
+                            hour1 = hourOfDay
+                            minute1 = minute
+                            "오전 " + hourOfDay + "시 " + minute + "분"
+                        }
+                        //selectTime = "${hourOfDay} 시 ${minute} 분"
+                        println("${selectFormattedTime}")
+                        println(hourOfDay)
+                        binding.setTime.text = selectTime
+                    }
+                }
+            val timePickerDialog = CustomTimePickerDialog(
+            this, myTimeListener
+        )
+
+                /*TimePickerDialog(
+                this,
+                //여기서 테마 설정해서 커스텀하기
+                android.R.style.ThemeHolo,
+                myTimeListener,
+                hour,
+                minute,
+                true
+            )*/
+
+            /*timePickerDialog.setTitle("시간 선택 :")
+            timePickerDialog.window!!.setBackgroundDrawableResource(android.R.color.transparent)
+            */
+            timePickerDialog.show()
+        }
+    }
+
+    private fun tempList() {
+        chipList.add(createNewChip("등교 a"))
+        chipList.add(createNewChip("흡연 b"))
+        chipList.add(createNewChip("남자"))
 
         for (i in chipList.indices) {
             // 마지막 Chip 뷰의 인덱스를 계산
@@ -29,12 +92,6 @@ class ChipAndBottomNavigationTestActivity : AppCompatActivity() {
                 binding.readSetFilterCg.addView(chipList[i])
             }
         }
-    }
-
-    private fun tempList() {
-        chipList.add(createNewChip("등교 a"))
-        chipList.add(createNewChip("흡연 b"))
-        chipList.add(createNewChip("남자"))
     }
 
 
---
+++ app/src/main/java/com/example/myappuidesignpractice/CustomTimePickerDialog.kt
@@ -0,0 +1,132 @@
+package com.example.myappuidesignpractice
+
+import android.app.TimePickerDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import android.widget.TimePicker
+import java.util.*
+
+class CustomTimePickerDialog(private val context: Context, private val t : TimePickerDialog.OnTimeSetListener) {
+    private var onTimeSetListener: TimePickerDialog.OnTimeSetListener? = t
+    private var h = 0
+    private var m = 0
+
+    fun show() {
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        val timePickerDialog = TimePickerDialog(
+            context,
+            t,
+            hour,
+            minute,
+            false
+        )
+        h = hour
+        m = minute
+
+
+        timePickerDialog.setButton(DialogInterface.BUTTON_POSITIVE, "확인") { dialog, which ->
+            /*val timePicker = (dialog as TimePickerDialog)
+            onTimeSetListener?.onTimeSet(
+                null,
+                h,
+                m
+            )*/
+            t
+        }
+
+        timePickerDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "취소") { dialog, which ->
+            dialog.dismiss()
+        }
+
+        timePickerDialog.setTitle("시간을 지정해주세요")
+        /*timePickerDialog.window!!.setBackgroundDrawableResource(android.R.color.transparent)
+        */timePickerDialog.show()
+    }
+
+    fun setOnTimeSetListener(listener: TimePickerDialog.OnTimeSetListener) {
+        onTimeSetListener = listener
+    }
+
+    /*private fun setCustomLayout() {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = inflater.inflate(R.layout.custom_time_picker_dialog, null)
+
+        // 제목 설정
+        val dialogTitle = view.findViewById<TextView>(R.id.dialogTitle)
+        dialogTitle.text = "Select Time"
+
+        // TimePicker 설정
+        val timePicker = view.findViewById<TimePicker>(R.id.timePicker)
+        timePicker.setIs24HourView(true)
+        timePicker.hour = initialHourOfDay
+        timePicker.minute = initialMinute
+
+        setView(view)
+    }*/
+
+    companion object {
+        private const val HOUR = "hour"
+        private const val MINUTE = "minute"
+
+        fun saveInstanceState(timePickerDialog: CustomTimePickerDialog): Bundle {
+            val bundle = Bundle()
+            val calendar = Calendar.getInstance()
+            bundle.putInt(HOUR, calendar.get(Calendar.HOUR_OF_DAY))
+            bundle.putInt(MINUTE, calendar.get(Calendar.MINUTE))
+            return bundle
+        }
+
+        fun restoreInstanceState(bundle: Bundle, context: Context, t : TimePickerDialog.OnTimeSetListener): CustomTimePickerDialog {
+            val timePickerDialog = CustomTimePickerDialog(context, t)
+            timePickerDialog.show()
+            return timePickerDialog
+        }
+    }
+}
+
+/*
+import android.app.TimePickerDialog
+import android.content.Context
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.TimePicker
+
+class com.example.myappuidesignpractice.CustomTimePickerDialog(
+    context: Context,
+    listener: TimePickerDialog.OnTimeSetListener?,
+    hourOfDay: Int,
+    minute: Int,
+    is24HourView: Boolean
+) : TimePickerDialog(context, listener, hourOfDay, minute, is24HourView) {
+
+    companion object {
+        private const val KEY_HOUR = "hour"
+        private const val KEY_MINUTE = "minute"
+        private const val KEY_IS_24_HOUR_VIEW = "is24HourView"
+    }
+
+    override fun onSaveInstanceState(): Bundle {
+        val bundle = super.onSaveInstanceState()
+        bundle.putInt(KEY_HOUR, timePicker.currentHour)
+        bundle.putInt(KEY_MINUTE, timePicker.currentMinute)
+        bundle.putBoolean(KEY_IS_24_HOUR_VIEW, timePicker.is24HourView)
+        return bundle
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val hour = savedInstanceState.getInt(KEY_HOUR)
+        val minute = savedInstanceState.getInt(KEY_MINUTE)
+        val is24HourView = savedInstanceState.getBoolean(KEY_IS_24_HOUR_VIEW)
+        updateTime(hour, minute)
+        setIs24HourView(is24HourView)
+    }
+}*/
